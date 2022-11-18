1b642b254beb782a11c2d60f6cbfbf5951eaced7
taufikmn17 <taufikmustafa60@gmail.com>
2022-11-18 15:23:51 +0700

    edit file

---
+++ app/src/main/java/com/example/tugaskelompok/ClubAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.tugaskelompok
+
+import androidx.appcompat.view.menu.MenuView.ItemView
+
+class ClubAdapter (private val clubList:ArrayList<DetailClub>) {
+    class DetailViewHolder(itemView: ItemView) : ReycycleView.ViewHolder()
+}
